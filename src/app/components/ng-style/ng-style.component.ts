5aa0faad1d3777e58142278a81ee7225abe0ae93
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-03-24 12:44:05 -0500

    ngClass

--- src/app/components/ng-style/ng-style.component.ts
+++ src/app/components/ng-style/ng-style.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   template: `
-  
+    // ngStyle 
     <p [style.fontSize.px]="tamano">
       Hola mundo.... esta es una etiqueta
     </p>
